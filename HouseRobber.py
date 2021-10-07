127082f05ddf145250db892d18b1184a710a036b
Audarya Uttarwar <audiuttarwar2000@gmail.com>
2021-10-07 13:36:20 +0530

    Add DP medium problem

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,3 +1,25 @@
+# Bottom Up
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        
+        dp = [-1 for i in range(n)]
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        
+        return dp[n-1]
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         a = 0
