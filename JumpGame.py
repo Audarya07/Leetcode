a9489e81abc4c153d8256cf6e6d09de160e12261
Audarya Uttarwar <audiuttarwar2000@gmail.com>
2021-10-11 16:29:19 +0530

    Add another approach for JumpGame

--- JumpGame.py
+++ JumpGame.py
@@ -5,3 +5,16 @@ class Solution:
             if i + nums[i] >= lastGoodIndex:
                 lastGoodIndex = i
         return lastGoodIndex == 0
+
+
+# Solution 2
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i in range(len(nums)):
+            if farthest < i:
+                return False
+            farthest = max(farthest, i + nums[i])
+        
+        return True
+ 
