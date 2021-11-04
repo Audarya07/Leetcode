491e7ecff14a3cee53b20b0f1e72efdc7e6bb73a
Audarya <audiuttarwar2000@gmail.com>
2021-11-05 02:09:47 +0530

    Added another solution to Intersection od 2 aray-II

--- IntersectionOfTwoArrays-II.py
+++ IntersectionOfTwoArrays-II.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+# Solution 1
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         m = len(nums1)
@@ -12,3 +13,16 @@ class Solution:
             ans.extend([val]*min(c1[val], c2[val]))
 
         return ans
+
+# Solution 2
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        arr = []
+        c1 = Counter(nums1)
+        
+        for x in nums2:
+            if x in c1 and c1[x] > 0:
+                arr.append(x)
+                c1[x] = c1[x] - 1
+        return arr
+    
